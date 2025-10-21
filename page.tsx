2119bb8f218819a9cc71e5bf5da9ad7edeb03e90
uticehockey27 <uticehockey27@gmail.com>
2025-10-21 10:39:06 -0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    // This is the template home page, please edit from this page
+    <div className="flex flex-col items-center pt-8">
+      <h1 className="text-xl font-bold">Please switch pages in the dropdown menu above 👆🏻</h1>
+    </div>
+  );
+}
